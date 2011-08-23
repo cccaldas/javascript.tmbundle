3fc57dc22ccf1b382898fb8a3586d0e92fcb4d40
Cristiano Caldas <eu@cccaldas.com>
2011-08-22 22:40:43 -0300

    validate json command added

---
+++ Support/bin/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+test.py
+
+Created by Cristiano Caldas on 2011-08-18.
+Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	print 'test.py'
+
+
+if __name__ == '__main__':
+	main()
+
